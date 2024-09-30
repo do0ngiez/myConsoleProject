75b4dcfcdfb02802975986cffefcff774830c58e
Kriszha Real <yujmin901205@gmail.com>
2024-10-01 01:51:19 +0800

    Add project files.

---
+++ myConsoleProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myConsoleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("What is your name?");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Hello, " + name + "!");
+            //Console.Read();
+
+            Console.WriteLine("Welcome to Acme Accounting Systems \nRemember, we're \"accounting\" on you");
+            Console.Read();
+        }
+    }
+}
+    
